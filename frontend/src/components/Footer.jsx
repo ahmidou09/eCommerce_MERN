6357cf50314fa930ef0a7eafb540bae18f63f79b
Ahmidou EL mehdi <ahmidouem@gmail.com>
2024-05-15 20:53:54 +0100

    Add styled Footer component with dynamic year

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import styled from "styled-components";
+
+const FooterContainer = styled.footer`
+  text-align: center;
+  padding: 2rem;
+  background-color: var(--color-primary-1);
+  color: var(--color-white);
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  font-size: 1.2rem;
+`;
+
+function Footer() {
+  const date = new Date().getFullYear();
+  return <FooterContainer>&copy; {date} all rights reserved</FooterContainer>;
+}
+
+export default Footer;
