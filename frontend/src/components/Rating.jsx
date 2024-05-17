54bb596efc0e959d4ae38aa4ebf2939ac138036a
Ahmidou EL mehdi <ahmidouem@gmail.com>
2024-05-17 13:09:39 +0100

    Add Rating component with star and review count display
    - Create `Rating` component to display filled, half, and empty stars based on the rating value
    - Utilize styled-components for `StarsContainer`, `Star`, `HalfStar`, `EmptyStar`, and `RatingNumber`

---
+++ frontend/src/components/Rating.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import styled from "styled-components";
+import { FaStar, FaStarHalfAlt } from "react-icons/fa";
+
+const StarsContainer = styled.div`
+  display: flex;
+  align-items: center;
+  padding-left: 8px;
+`;
+
+const Star = styled(FaStar)`
+  color: var(--color-accent-4);
+  margin-right: 2px;
+`;
+
+const HalfStar = styled(FaStarHalfAlt)`
+  color: var(--color-accent-4);
+  margin-right: 2px;
+`;
+
+const EmptyStar = styled(FaStar)`
+  color: var(--color-grey-0);
+  margin-right: 2px;
+`;
+
+const RatingNumber = styled.span`
+  margin-left: 8px;
+  font-size: 1em;
+  color: var(--color-grey-0);
+`;
+
+const Rating = ({ rating, totalReviews }) => {
+  const filledStars = Math.floor(rating);
+  const hasHalfStar = rating % 1 !== 0;
+
+  return (
+    <StarsContainer>
+      {Array.from({ length: filledStars }, (v, i) => (
+        <Star key={`full-${i}`} />
+      ))}
+      {hasHalfStar && <HalfStar key="half" />}
+      {Array.from(
+        { length: 5 - filledStars - (hasHalfStar ? 1 : 0) },
+        (v, i) => (
+          <EmptyStar key={`empty-${i}`} />
+        )
+      )}
+      <RatingNumber>({totalReviews})</RatingNumber>
+    </StarsContainer>
+  );
+};
+
+export default Rating;
