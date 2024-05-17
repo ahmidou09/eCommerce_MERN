5789c577122855ebb08b399fa6343da952ef3e00
Ahmidou EL mehdi <ahmidouem@gmail.com>
2024-05-17 13:08:04 +0100

    Create `AppLayout` component using React and styled-components

---
+++ frontend/src/ui/AppLayout.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+import styled from "styled-components";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+
+const StyledAppLayout = styled.div`
+  width: 100%;
+`;
+
+function AppLayout() {
+  return (
+    <StyledAppLayout>
+      <Navbar />
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
+    </StyledAppLayout>
+  );
+}
+
+export default AppLayout;
