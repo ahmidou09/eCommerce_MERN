1c6f86a5e75537a01c90b6cd66315128cd8d294f
Ahmidou EL mehdi <ahmidouem@gmail.com>
2024-05-24 20:53:32 +0100

    Setup Express server with MongoDB connection
    - Import and configure `dotenv` for environment variables
    - Import and connect to MongoDB using `connectDB`
    - Import and configure Express, CORS, and product routes
    - Setup error handling middleware
    - Start server on the specified port
    - Add a default route to confirm API is running

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+import connectDB from "./config/db.js";
+import cors from "cors";
+import productRoutes from "./routes/productRoutes.js";
+import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
+
+const port = process.env.PORT || 5000;
+
+connectDB();
+
+const app = express();
+app.use(cors());
+app.use("/api/products", productRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
