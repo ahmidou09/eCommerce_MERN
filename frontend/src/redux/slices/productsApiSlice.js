8c9a993f8117a4e9b094f0ecc551670e5d472218
Ahmidou EL mehdi <ahmidouem@gmail.com>
2024-05-27 20:33:58 +0100

    Add product endpoints to apiSlice: Implemented getProducts and getProductById queries

---
+++ frontend/src/redux/slices/productsApiSlice.js
@@ -0,0 +1,22 @@
+import { apiSlice } from "./apiSlice";
+import { BASE_URL, PRODUCTS_URL } from "../../constants";
+
+export const productsApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    getProducts: builder.query({
+      query: () => ({
+        url: `${BASE_URL}${PRODUCTS_URL}`,
+        method: "GET",
+      }),
+    }),
+    getProductById: builder.query({
+      query: (id) => ({
+        url: `${BASE_URL}${PRODUCTS_URL}/${id}`,
+        method: "GET",
+      }),
+    }),
+    keepUnusedDataFor: 5,
+  }),
+});
+
+export const { useGetProductsQuery, useGetProductByIdQuery } = productsApiSlice;
