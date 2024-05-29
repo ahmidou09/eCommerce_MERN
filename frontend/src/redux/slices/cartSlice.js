3615b1670876447283ac59838e09307c5567b6c6
Ahmidou EL mehdi <ahmidouem@gmail.com>
2024-05-29 19:37:02 +0100

    Refactor cartSlice to use updateCart utility function for consistent cart state calculations

--- frontend/src/redux/slices/cartSlice.js
+++ frontend/src/redux/slices/cartSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { updateCart } from "../../utils/cartUtil";
 
 const initialState = localStorage.getItem("cart")
   ? JSON.parse(localStorage.getItem("cart"))
@@ -6,11 +7,11 @@ const initialState = localStorage.getItem("cart")
       cartItems: [],
       shippingAddress: {},
       paymentMethod: "",
+      itemsPrice: 0,
+      shippingPrice: 0,
+      taxPrice: 0,
+      totalPrice: 0,
     };
-
-const addDecimals = (num) => {
-  return (Math.round(num * 100) / 100).toFixed(2);
-};
 
 const cartSlice = createSlice({
   name: "cart",
@@ -21,39 +22,13 @@ const cartSlice = createSlice({
       const existItem = state.cartItems.find((x) => x._id === item._id);
       if (existItem) {
         state.cartItems = state.cartItems.map((x) =>
-          x._id === existItem._id
-            ? { ...item, quantity: existItem.quantity + item.quantity }
-            : x
+          x._id === existItem._id ? { ...x, quantity: item.quantity } : x
         );
       } else {
         state.cartItems = [...state.cartItems, item];
       }
 
-      // Calculate Items price
-      state.itemsPrice = addDecimals(
-        state.cartItems.reduce(
-          (acc, item) => acc + item.price * item.quantity,
-          0
-        )
-      );
-
-      // Calculate shipping price
-      state.shippingPrice = state.itemsPrice > 100 ? 0 : 10;
-
-      // Calculate tax price
-      state.taxPrice = addDecimals(
-        Number((0.15 * state.itemsPrice).toFixed(2))
-      );
-
-      // Calculate total price
-      state.totalPrice = Number(
-        parseFloat(state.itemsPrice) +
-          parseFloat(state.shippingPrice) +
-          parseFloat(state.taxPrice)
-      ).toFixed(2);
-
-      // Save cart to localStorage
-      localStorage.setItem("cart", JSON.stringify(state));
+      return updateCart(state);
     },
   },
 });
