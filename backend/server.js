40d0ea918c2fc3451a0f965c6526ec9b5706c6cb
Ahmidou EL mehdi <ahmidouem@gmail.com>
2024-06-02 14:48:36 +0100

    Add userRoutes and body parsers to server

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ dotenv.config();
 import connectDB from "./config/db.js";
 import cors from "cors";
 import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
 
 const port = process.env.PORT || 5000;
@@ -11,8 +12,11 @@ const port = process.env.PORT || 5000;
 connectDB();
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
