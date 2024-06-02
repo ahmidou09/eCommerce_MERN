48d930df048d460bb4a9cfab45b49c148bd6e798
Ahmidou EL mehdi <ahmidouem@gmail.com>
2024-06-02 14:48:19 +0100

    Set up user routes for authentication and profile management

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,45 @@
+import express from "express";
+const router = express.Router();
+import {
+  loginUser,
+  registerUser,
+  logoutUser,
+  getUserProfile,
+  updateUserProfile,
+  getUsers,
+  getUserById,
+  deleteUser,
+  updateUser,
+} from "../controllers/userController.js";
+
+// @desc    get all users and register new user
+// @route   GET /api/users OR POST /api/users
+// @access  private/admin
+router.route("/").post(registerUser).get(getUsers);
+
+// @desc    user login and get token
+// @route   POST /api/users/login
+// @access  Public
+router.post("/login", loginUser);
+
+// @desc    user register
+// @route   POST /api/users
+// @access  Public
+router.post("/register", registerUser);
+
+// @desc    user logout and clear cookie
+// @route   POST /api/users/logout
+// @access  private
+router.post("/logout", logoutUser);
+
+// @desc    update user profile and get user profile
+// @route   PUT /api/users/profile OR GET /api/users/profile
+// @access  private
+router.route("/profile").put(updateUserProfile).get(getUserProfile);
+
+// @desc    update user and delete user and get user by id
+// @route   PUT DELETE GET /api/users/:id
+// @access  private/admin
+router.route("/:id").put(updateUser).delete(deleteUser).get(getUserById);
+
+export default router;
