14650cb4ce3cd29cb4275770bc42b5c8e80617dd
Ahmidou EL mehdi <ahmidouem@gmail.com>
2024-06-03 14:37:39 +0100

    Protect user routes with authentication and admin middleware.

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -11,11 +11,12 @@ import {
   deleteUser,
   updateUser,
 } from "../controllers/userController.js";
+import { protect, admin } from "../middleware/authMiddleware.js";
 
 // @desc    get all users and register new user
 // @route   GET /api/users OR POST /api/users
 // @access  private/admin
-router.route("/").post(registerUser).get(getUsers);
+router.route("/").post(registerUser).get(protect, admin, getUsers);
 
 // @desc    user login and get token
 // @route   POST /api/users/login
@@ -25,7 +26,7 @@ router.post("/login", loginUser);
 // @desc    user register
 // @route   POST /api/users
 // @access  Public
-router.post("/register", registerUser);
+// router.post("/register", protect, registerUser);
 
 // @desc    user logout and clear cookie
 // @route   POST /api/users/logout
@@ -35,11 +36,18 @@ router.post("/logout", logoutUser);
 // @desc    update user profile and get user profile
 // @route   PUT /api/users/profile OR GET /api/users/profile
 // @access  private
-router.route("/profile").put(updateUserProfile).get(getUserProfile);
+router
+  .route("/profile")
+  .put(protect, updateUserProfile)
+  .get(protect, getUserProfile);
 
 // @desc    update user and delete user and get user by id
 // @route   PUT DELETE GET /api/users/:id
 // @access  private/admin
-router.route("/:id").put(updateUser).delete(deleteUser).get(getUserById);
+router
+  .route("/:id")
+  .put(protect, admin, updateUser)
+  .delete(protect, admin, deleteUser)
+  .get(protect, admin, getUserById);
 
 export default router;
