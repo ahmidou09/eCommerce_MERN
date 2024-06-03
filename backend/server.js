0d061f6420247f59e375bad5d3808c5ef4108e7d
Ahmidou EL mehdi <ahmidouem@gmail.com>
2024-06-03 14:37:50 +0100

    Add cookie parser middleware and error handling to the server setup.

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 dotenv.config();
 import connectDB from "./config/db.js";
@@ -7,24 +8,39 @@ import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
 
+// port
 const port = process.env.PORT || 5000;
 
+// connect to database
 connectDB();
 
+// express app
 const app = express();
+
+// body parser middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// cors middleware
 app.use(cors());
+
+// cookies parser middleware
+app.use(cookieParser());
+
+// routes
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 
+// error middleware
 app.use(notFound);
 app.use(errorHandler);
 
+// server listen
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
 
+// test route
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
