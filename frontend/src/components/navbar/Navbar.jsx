c8c2bceac928e70efbac46270cd52a597f14f02f
Ahmidou EL mehdi <ahmidouem@gmail.com>
2024-06-04 13:47:01 +0100

    Refactor Navbar with styled-components for better structure and readability.

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -2,6 +2,10 @@ import React, { useState } from "react";
 import styled from "styled-components";
 import { FaRegHeart, FaRegUser } from "react-icons/fa";
 import { AiOutlineShoppingCart } from "react-icons/ai";
+import { CgShoppingBag } from "react-icons/cg";
+import { ImCancelCircle } from "react-icons/im";
+import { FaRegStar } from "react-icons/fa";
+import { RiLogoutBoxLine } from "react-icons/ri";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 
@@ -15,101 +19,105 @@ const NavbarContainer = styled.nav`
   justify-content: space-between;
   align-items: center;
   padding: 2.5rem 2rem;
+`;
 
-  .logo {
+const Logo = styled.div`
+  span {
+    font-size: 1.8rem;
+    font-weight: bold;
+  }
+`;
+
+const NavLinks = styled.ul`
+  display: flex;
+  align-items: center;
+  gap: 2rem;
+
+  a {
+    text-decoration: none;
+    color: var(--color-black);
+    font-size: 1.6rem;
+    cursor: pointer;
+    transition: all 0.3s;
+    &:hover {
+      color: var(--color-primary-1);
+    }
+  }
+`;
+
+const NavIcons = styled.ul`
+  display: flex;
+  align-items: center;
+  gap: 2rem;
+`;
+
+const IconContainer = styled.div`
+  position: relative;
+  cursor: pointer;
+`;
+
+const IconCount = styled.span`
+  position: absolute;
+  top: -0.9rem;
+  right: -0.9rem;
+  width: 1.6rem;
+  height: 1.6rem;
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: var(--color-white);
+  padding: 0.5rem;
+  background-color: var(--color-primary-1);
+  font-size: 1.2rem;
+`;
+
+const UserIcon = styled(IconContainer)`
+  background-color: var(--color-primary-1);
+  border-radius: 50%;
+  width: 3rem;
+  height: 3rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  transform: translateY(-4px);
+
+  svg {
+    color: var(--color-white);
+    font-size: 1.6rem !important;
+  }
+`;
+
+const DropdownMenu = styled.div`
+  position: absolute;
+  top: 4rem;
+  right: 0;
+  background-color: #808e9b;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  border-radius: 0.5rem;
+  overflow: hidden;
+  z-index: 10;
+  display: flex;
+  flex-direction: column;
+  width: 20rem;
+`;
+
+const DropdownItem = styled(Link)`
+  padding: 1rem;
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+  color: var(--color-black);
+  text-decoration: none;
+  transition: all 0.3s;
+
+  &:hover {
+    background-color: var(--color-primary-1);
+    color: var(--color-white);
   }
 
-  .navbar {
-    &_links {
-      display: flex;
-      align-items: center;
-      gap: 2rem;
-    }
-
-    &_link {
-      cursor: pointer;
-    }
-
-    &_icons {
-      display: flex;
-      align-items: center;
-      gap: 2rem;
-      cursor: pointer;
-
-      &_user {
-        position: relative;
-        background-color: var(--color-primary-1);
-        border-radius: 50%;
-        width: 3rem;
-        height: 3rem;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        transform: translateY(-4px);
-
-        svg {
-          color: var(--color-white);
-          font-size: 1.6rem !important;
-        }
-      }
-
-      &_dropdown {
-        position: absolute;
-        top: 3.5rem;
-        right: 0;
-        background-color: white;
-        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-        border-radius: 0.5rem;
-        overflow: hidden;
-        z-index: 10;
-        display: flex;
-        flex-direction: column;
-        width: 15rem;
-      }
-
-      &_dropdown_item {
-        padding: 1rem;
-        display: flex;
-        align-items: center;
-        gap: 1rem;
-        color: var(--color-black);
-        text-decoration: none;
-
-        &:hover {
-          background-color: var(--color-primary-1);
-          color: var(--color-white);
-        }
-
-        svg {
-          font-size: 1.4rem;
-        }
-      }
-    }
-
-    &_icon {
-      &_container {
-        position: relative;
-      }
-      &_count {
-        position: absolute;
-        top: -0.9rem;
-        right: -0.9rem;
-        width: 1.6rem;
-        height: 1.6rem;
-        border-radius: 50%;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        color: var(--color-white);
-        padding: 0.5rem;
-        background-color: var(--color-primary-1);
-        font-size: 1.2rem;
-      }
-
-      svg {
-        font-size: 2rem;
-      }
-    }
+  svg {
+    font-size: 1.4rem;
   }
 `;
 
@@ -121,9 +129,6 @@ const Container = styled.div`
 function Navbar() {
   const { cartItems } = useSelector((state) => state.cart);
   const { wishListItems } = useSelector((state) => state.wishList);
-  const cartItemsCount = cartItems.length;
-  const wishListItemsCount = wishListItems.length;
-
   const [dropdownOpen, setDropdownOpen] = useState(false);
 
   const toggleDropdown = () => {
@@ -134,84 +139,70 @@ function Navbar() {
     <NavBar>
       <Container>
         <NavbarContainer>
-          <div className="logo">
+          <Logo>
             <Link to="/">
               <span>eCommerceShop</span>
             </Link>
-          </div>
-          <ul className="navbar_links">
-            <li className="navbar_link">
+          </Logo>
+          <NavLinks>
+            <li>
               <Link to="/">Home</Link>
             </li>
-            <li className="navbar_link">
+            <li>
               <Link to="/contact">Contact</Link>
             </li>
-            <li className="navbar_link">
+            <li>
               <Link to="/about">About</Link>
             </li>
-            <li className="navbar_link">
+            <li>
               <Link to="/signup">Sign Up</Link>
             </li>
-          </ul>
-
-          <ul className="navbar_icons">
-            <li className="navbar_icon">
+          </NavLinks>
+          <NavIcons>
+            <IconContainer>
               <Link to="/wishList">
-                <div className="navbar_icon_container">
-                  {wishListItemsCount > 0 && (
-                    <span className="navbar_icon_count">
-                      {wishListItemsCount}
-                    </span>
-                  )}
-                  <FaRegHeart />
-                </div>
+                {wishListItems.length > 0 && (
+                  <IconCount>{wishListItems.length}</IconCount>
+                )}
+                <FaRegHeart />
               </Link>
-            </li>
-
-            <li className="navbar_icon">
+            </IconContainer>
+            <IconContainer>
               <Link to="/cart">
-                <div className="navbar_icon_container">
-                  {cartItemsCount > 0 && (
-                    <span className="navbar_icon_count">{cartItemsCount}</span>
-                  )}
-                  <AiOutlineShoppingCart />
-                </div>
+                {cartItems.length > 0 && (
+                  <IconCount>{cartItems.length}</IconCount>
+                )}
+                <AiOutlineShoppingCart />
               </Link>
-            </li>
-            <li
-              className="navbar_icon navbar_icons_user"
-              onClick={toggleDropdown}
-            >
+            </IconContainer>
+            <UserIcon onClick={toggleDropdown}>
               <FaRegUser />
               {dropdownOpen && (
-                <div className="navbar_icons_dropdown">
-                  <Link to="/account" className="navbar_icons_dropdown_item">
+                <DropdownMenu>
+                  <DropdownItem to="/account">
                     <FaRegUser />
                     Manage My Account
-                  </Link>
-                  <Link to="/orders" className="navbar_icons_dropdown_item">
-                    <AiOutlineShoppingCart />
+                  </DropdownItem>
+                  <DropdownItem to="/orders">
+                    <CgShoppingBag />
                     My Order
-                  </Link>
-                  <Link
-                    to="/cancellations"
-                    className="navbar_icons_dropdown_item"
-                  >
-                    <FaRegHeart />
+                  </DropdownItem>
+                  <DropdownItem to="/cancellations">
+                    <ImCancelCircle />
                     My Cancellations
-                  </Link>
-                  <Link to="/reviews" className="navbar_icons_dropdown_item">
-                    <FaRegHeart />
+                  </DropdownItem>
+                  <DropdownItem to="/reviews">
+                    <FaRegStar />
                     My Reviews
-                  </Link>
-                  <Link to="/logout" className="navbar_icons_dropdown_item">
-                    <FaRegUser />
+                  </DropdownItem>
+                  <DropdownItem to="/logout">
+                    <RiLogoutBoxLine />
                     Logout
-                  </Link>
-                </div>
+                  </DropdownItem>
+                </DropdownMenu>
               )}
-            </li>
-          </ul>
+            </UserIcon>
+          </NavIcons>
         </NavbarContainer>
       </Container>
     </NavBar>
