c918c2064975e574050f4b4853e4d82eb66afdad
Ahmidou EL mehdi <ahmidouem@gmail.com>
2024-06-05 18:36:40 +0100

    Rename login action and update authSlice actions
    - Rename login action to setCredentials.
    - Update logout action to remove userInfo from local storage.

--- frontend/src/redux/slices/authSlice.js
+++ frontend/src/redux/slices/authSlice.js
@@ -10,15 +10,16 @@ const authSlice = createSlice({
   name: "auth",
   initialState,
   reducers: {
-    login: (state, action) => {
+    setCredentials: (state, action) => {
       state.userInfo = action.payload;
       localStorage.setItem("userInfo", JSON.stringify(action.payload));
     },
-    logout: (state) => {
+    logout: (state, action) => {
       state.userInfo = null;
+      localStorage.removeItem("userInfo");
     },
   },
 });
 
-export const { login, logout } = authSlice.actions;
+export const { setCredentials, logout } = authSlice.actions;
 export default authSlice.reducer;
