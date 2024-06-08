8d88f621b101060732b88bad065f826ab8833d65
Ahmidou EL mehdi <ahmidouem@gmail.com>
2024-06-08 13:55:48 +0100

    Imported and configured order routes in `server.js`.

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import connectDB from "./config/db.js";
 import cors from "cors";
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
 import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
 
 // port
@@ -30,6 +31,7 @@ app.use(cookieParser());
 // routes
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/orders", orderRoutes);
 
 // error middleware
 app.use(notFound);
