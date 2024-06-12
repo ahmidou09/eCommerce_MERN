09a60e7374ddb37fbadf24934cb897e32c5e55f0
Ahmidou EL mehdi <ahmidouem@gmail.com>
2024-06-12 13:35:29 +0100

    Removed unnecessary `console.error` in `authMiddleware.js`

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -11,7 +11,6 @@ const protect = asyncHandler(async (req, res, next) => {
       req.user = await User.findById(decoded.userId).select("-password");
       next();
     } catch (error) {
-      console.error(error);
       res.status(401);
       throw new Error("Not authorized, token failed");
     }
