417a7763a12909fa62725f635884ade53715aaa1
Ahmidou EL mehdi <ahmidouem@gmail.com>
2024-06-12 21:03:00 +0100

    Implement logic to update order status to paid in `orderController.js`.

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -65,14 +65,31 @@ const getOrderById = asyncHandler(async (req, res) => {
 });
 
 // @desc    update order to paid
-// @route   GET /api/orders/:id/pay
+// @route   PUT /api/orders/:id/pay
 // @access  admin
 const updateOrderToPaid = asyncHandler(async (req, res) => {
-  res.send("update Order To Paid");
+  const order = await Order.findById(req.params.id);
+
+  if (order) {
+    order.isPaid = true;
+    order.paidAt = Date.now();
+    order.paymentResult = {
+      id: req.body.id,
+      status: req.body.status,
+      update_time: req.body.update_time,
+      email_address: req.body.email_address,
+    };
+
+    const updatedOrder = await order.save();
+    res.status(200).json(updatedOrder);
+  } else {
+    res.status(404);
+    throw new Error("order not found");
+  }
 });
 
 // @desc    update order to delivered
-// @route   GET /api/orders/:id/deliver
+// @route   PUT /api/orders/:id/deliver
 // @access  admin
 const updateOrderToDelivered = asyncHandler(async (req, res) => {
   res.send("update Order To Delivered");
