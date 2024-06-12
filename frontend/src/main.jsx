b71477c9490cd500380fae2b8520c67ed751c477
Ahmidou EL mehdi <ahmidouem@gmail.com>
2024-06-12 21:03:38 +0100

    Wrap the application in `PayPalScriptProvider` in `main.jsx`.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import { PayPalScriptProvider } from "@paypal/react-paypal-js";
 import { Provider } from "react-redux";
 import store from "./redux/store.js";
 import GlobalStyles from "./styles/GlobalStyles.js";
@@ -8,8 +9,10 @@ import GlobalStyles from "./styles/GlobalStyles.js";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <GlobalStyles />
-      <App />
+      <PayPalScriptProvider deferLoading={true}>
+        <GlobalStyles />
+        <App />
+      </PayPalScriptProvider>
     </Provider>
   </React.StrictMode>
 );
