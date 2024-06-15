5f9b466f9d89ebbafc17354fb563404276c3a249
Ahmidou EL mehdi <ahmidouem@gmail.com>
2024-06-15 12:20:17 +0100

    Added CRUD operation handlers (create, update, delete) in `productController.js`.

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,10 +1,18 @@
 import asyncHandler from "../middleware/asyncHandler.js";
 import Product from "../models/productModel.js";
 
+// @ desc    get all products
+// @ route   GET /api/products
+// @ access  public
+
 const getProducts = asyncHandler(async (req, res) => {
   const products = await Product.find({});
   res.json(products);
 });
+
+// @ desc    get single product
+// @ route   GET /api/products/:id
+// @ access  public
 
 const getProductById = asyncHandler(async (req, res) => {
   const product = await Product.findById(req.params.id);
@@ -16,4 +24,34 @@ const getProductById = asyncHandler(async (req, res) => {
   }
 });
 
-export { getProducts, getProductById };
+// @ desc    delete product
+// @ route   DELETE /api/products/:id
+// @ access  private/admin
+
+const deleteProduct = asyncHandler(async (req, res) => {
+  res.send("delete product");
+});
+
+// @ desc    create product
+// @ route   POST /api/products
+// @ access  private/admin
+
+const createProduct = asyncHandler(async (req, res) => {
+  res.send("create product");
+});
+
+// @ desc    update product
+// @ route   PUT /api/products/:id
+// @ access  private/admin
+
+const updateProduct = asyncHandler(async (req, res) => {
+  res.send("update product");
+});
+
+export {
+  getProducts,
+  getProductById,
+  deleteProduct,
+  createProduct,
+  updateProduct,
+};
