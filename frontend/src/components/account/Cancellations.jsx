40190e04e2b3e3f5471b4c9cddbc517484e6c547
Ahmidou EL mehdi <ahmidouem@gmail.com>
2024-06-16 17:15:39 +0100

    Refactor `Cancellations.jsx` and `MyOrders.jsx` to use `TableItems` component for displaying orders.

--- frontend/src/components/account/Cancellations.jsx
+++ frontend/src/components/account/Cancellations.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import { useGetMyOrdersQuery } from "../../redux/slices/ordersApiSlice";
-import { Link } from "react-router-dom";
 import styled from "styled-components";
+import { useGetMyOrdersQuery } from "../../redux/slices/ordersApiSlice";
 import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
 import { truncateString } from "../../utils/cartUtil";
+import TableItems from "../ui/TableItems";
+import { Link } from "react-router-dom";
 
 const Cancellations = () => {
   const {
@@ -12,6 +13,54 @@ const Cancellations = () => {
     isLoading: loadingOrders,
     error: errorOrders,
   } = useGetMyOrdersQuery();
+
+  const filteredOrders = orders?.filter((order) => !order.isPaid);
+
+  const columns = [
+    { key: "_id", title: "Order ID" },
+    { key: "image", title: "Image" },
+    { key: "name", title: "Name" },
+    { key: "isPaid", title: "Paid" },
+    { key: "isDelivered", title: "Delivered" },
+    { key: "totalPrice", title: "Total Price" },
+    { key: "createdAt", title: "Date" },
+    { key: "actions", title: "" },
+  ];
+
+  const renderItem = (order) => (
+    <tr key={order._id}>
+      <td>{truncateString(order._id, 10)}</td>
+      <td>
+        <img
+          src={order.orderItems[0].image}
+          alt={order.orderItems[0].name}
+          width="50"
+        />
+      </td>
+      <td>{truncateString(order.orderItems[0].name, 10)}</td>
+      <td>
+        {order.isPaid ? (
+          new Date(order.paidAt).toLocaleDateString()
+        ) : (
+          <span>✕</span>
+        )}
+      </td>
+      <td>
+        {order.isDelivered ? (
+          new Date(order.deliveredAt).toLocaleDateString()
+        ) : (
+          <span>✕</span>
+        )}
+      </td>
+      <td>${order.totalPrice.toFixed(2)}</td>
+      <td>{new Date(order.createdAt).toLocaleDateString()}</td>
+      <td>
+        <button>
+          <Link to={`/orders/${order._id}`}>Detail</Link>
+        </button>
+      </td>
+    </tr>
+  );
 
   return (
     <Container>
@@ -21,59 +70,12 @@ const Cancellations = () => {
         <div>Error: {errorOrders.message}</div>
       ) : (
         <>
-          <h2>Orders Cancellations ff</h2>
-          <OrdersTable>
-            <thead>
-              <tr>
-                <th>Order ID</th>
-                <th>Image</th>
-                <th>Name</th>
-                <th>Paid</th>
-                <th>Delivered</th>
-                <th>Total Price</th>
-                <th>Date</th>
-                <th></th>
-              </tr>
-            </thead>
-            <tbody>
-              {orders
-                ?.filter((order) => !order.isPaid)
-                .map((order) => (
-                  <tr key={order._id}>
-                    <td> {truncateString(order._id, 10)}</td>
-                    <td>
-                      <img
-                        src={order.orderItems[0].image}
-                        alt={order.orderItems[0].name}
-                        width="50"
-                      />
-                    </td>
-                    <td>{truncateString(order.orderItems[0].name, 10)}</td>
-                    <td>
-                      {order.isPaid ? (
-                        new Date(order.paidAt).toLocaleDateString()
-                      ) : (
-                        <span>✕</span>
-                      )}
-                    </td>
-                    <td>
-                      {order.isDelivered ? (
-                        new Date(order.deliveredAt).toLocaleDateString()
-                      ) : (
-                        <span>✕</span>
-                      )}
-                    </td>
-                    <td>${order.totalPrice.toFixed(2)}</td>
-                    <td>{new Date(order.createdAt).toLocaleDateString()}</td>
-                    <td>
-                      <button>
-                        <Link to={`/orders/${order._id}`}>detail </Link>
-                      </button>
-                    </td>
-                  </tr>
-                ))}
-            </tbody>
-          </OrdersTable>
+          <h1>Cancellations</h1>
+          <TableItems
+            data={filteredOrders}
+            columns={columns}
+            renderItem={renderItem}
+          />
         </>
       )}
     </Container>
@@ -83,46 +85,28 @@ const Cancellations = () => {
 const Container = styled.div`
   max-width: 1200px;
   margin: 0 auto;
-  height: 85vh;
   padding: 2rem;
-`;
+  min-height: 85vh;
 
-const OrdersTable = styled.table`
-  width: 100%;
-  border-collapse: collapse;
-  margin-top: 2rem;
-
-  th,
   td {
-    border: 1px solid var(--color-grey-1);
-    padding: 1rem;
-    text-align: center;
+    text-align: left;
+
+    button {
+      background-color: var(--color-grey-1);
+      border: none;
+      border-radius: 5px;
+      padding: 0.5rem 1rem;
+      cursor: pointer;
+
+      &:hover {
+        background-color: var(--color-grey-4);
+      }
+    }
 
     span {
       font-weight: 600;
       color: var(--color-primary-1);
     }
-
-    button {
-      background-color: var(--color-grey-2);
-      border: none;
-      border-radius: 5px;
-      padding: 0.5rem 1rem;
-      cursor: pointer;
-      transition: all 0.3s ease;
-
-      &:hover {
-        background-color: var(--color-grey-1);
-      }
-    }
-  }
-
-  th {
-    background-color: var(--color-grey-2);
-  }
-
-  img {
-    max-width: 50px;
   }
 `;
 
