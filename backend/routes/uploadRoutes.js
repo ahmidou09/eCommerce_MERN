8a63739a82c2be21eb939ec059f26f2f283cf882
Ahmidou EL mehdi <ahmidouem@gmail.com>
2024-06-17 17:19:44 +0100

    Created `uploadRoutes.js` for managing image uploads and storing them in the `uploads` directory.

---
+++ backend/routes/uploadRoutes.js
@@ -0,0 +1,52 @@
+import path from "path";
+import express from "express";
+import multer from "multer";
+
+const router = express.Router();
+
+const storage = multer.diskStorage({
+  destination(req, file, cb) {
+    console.log("Destination:", "uploads/");
+    cb(null, "uploads/");
+  },
+  filename(req, file, cb) {
+    console.log("Original filename:", file.originalname);
+    cb(
+      null,
+      `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`
+    );
+  },
+});
+
+function fileFilter(req, file, cb) {
+  const filetypes = /jpe?g|png|webp/;
+  const mimetypes = /image\/jpe?g|image\/png|image\/webp/;
+
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+  const mimetype = mimetypes.test(file.mimetype);
+
+  if (extname && mimetype) {
+    cb(null, true);
+  } else {
+    cb(new Error("Images only!"), false);
+  }
+}
+
+const upload = multer({ storage, fileFilter });
+const uploadSingleImage = upload.single("image");
+
+router.post("/", (req, res) => {
+  uploadSingleImage(req, res, function (err) {
+    if (err) {
+      console.error("Upload Error:", err);
+      return res.status(400).send({ message: err.message });
+    }
+
+    res.status(200).send({
+      message: "Image uploaded successfully",
+      image: `/${req.file.path}`,
+    });
+  });
+});
+
+export default router;
