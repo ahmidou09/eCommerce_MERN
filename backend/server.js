8528cd3525fb08e2c4b5257a08013cd0bc9cc823
Ahmidou EL mehdi <ahmidouem@gmail.com>
2024-06-17 17:20:09 +0100

    Added upload routes for handling image uploads in `server.js`.

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
@@ -8,6 +9,7 @@ import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
 import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
+import uploadRoutes from "./routes/uploadRoutes.js";
 
 // port
 const port = process.env.PORT || 5000;
@@ -31,6 +33,11 @@ app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
+app.use("/api/upload", uploadRoutes);
+
+// serve static assets in production
+const __dirname = path.resolve();
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
 // payment route
 app.get("/api/config/paypal", (req, res) => {
