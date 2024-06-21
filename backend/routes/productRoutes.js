b1f70311c756693e2eed396f5760e8bc493029b5
Ahmidou EL mehdi <ahmidouem@gmail.com>
2024-06-21 18:27:00 +0100

    Add a new route and controller method `getTopProducts` to fetch top-rated products.

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -8,10 +8,12 @@ import {
   updateProduct,
   createProductReview,
   getMyReviews,
+  getTopProducts,
 } from "../controllers/productController.js";
 import { protect, admin } from "../middleware/authMiddleware.js";
 
 router.route("/").get(getProducts).post(protect, admin, createProduct);
+router.route("/top").get(getTopProducts);
 router.route("/myreviews").get(protect, getMyReviews);
 router
   .route("/:id")
