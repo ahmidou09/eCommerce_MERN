4ca80a2dedc3452c09667ed50bce388dcaf9f47c
Ahmidou EL mehdi <ahmidouem@gmail.com>
2024-06-21 18:29:42 +0100

    Introduce `Paginate` component for pagination UI

---
+++ frontend/src/components/ui/Paginate.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+const Paginate = ({ pages, page, root }) => {
+  return (
+    pages > 1 && (
+      <PaginationContainer>
+        {[...Array(pages).keys()].map((x) => (
+          <PaginationItem
+            key={x + 1}
+            to={`/${root}/${x + 1}`}
+            className={x + 1 === page ? "active" : ""}
+          >
+            {x + 1}
+          </PaginationItem>
+        ))}
+      </PaginationContainer>
+    )
+  );
+};
+
+const PaginationContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 1rem;
+`;
+
+const PaginationItem = styled(Link)`
+  padding: 0.5rem 1rem;
+  margin: 0 0.25rem;
+  border: 1px solid var(--color-grey-2);
+  border-radius: 4px;
+  background-color: var(--color-white-2);
+  color: var(--color-primary-2);
+  text-decoration: none;
+  cursor: pointer;
+
+  &.active {
+    background-color: var(--color-primary-1);
+    color: var(--color-white-2);
+  }
+
+  &:hover {
+    background-color: var(--color-white-2);
+    color: var(--color-primary-2);
+  }
+
+  &.active:hover {
+    background-color: var(--color-primary-2);
+    color: var(--color-white-2);
+  }
+`;
+
+export default Paginate;
