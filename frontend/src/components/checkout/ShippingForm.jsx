5e487abc1c08ba676a02c6043b0f4ba459d4cc8f
Ahmidou EL mehdi <ahmidouem@gmail.com>
2024-06-22 19:50:06 +0100

    setup to deploy

---
+++ frontend/src/components/checkout/ShippingForm.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import styled from "styled-components";
+import FormFields from "../ui/FormFields";
+
+const ShippingForm = ({
+  formFields,
+  handleInputChange,
+  saveShipping,
+  handleCheckboxChange,
+}) => {
+  const formFieldsShipping = [
+    { id: "address", label: "Address", type: "text", required: true },
+    { id: "city", label: "City", type: "text", required: true },
+    { id: "postalCode", label: "Code Postal", type: "text", required: true },
+    { id: "country", label: "Country", type: "text", required: true },
+  ];
+
+  return (
+    <ShipingFormWrapper>
+      <CheckoutFormTitle>Shipping Details</CheckoutFormTitle>
+      <FormFields
+        fields={formFieldsShipping}
+        formFields={formFields}
+        handleInputChange={handleInputChange}
+      />
+      <SaveShipping>
+        <input
+          type="checkbox"
+          id="saveShippingAddress"
+          checked={saveShipping}
+          onChange={handleCheckboxChange}
+        />
+        <label htmlFor="saveShippingAddress">Save Shipping Address</label>
+      </SaveShipping>
+    </ShipingFormWrapper>
+  );
+};
+
+const ShipingFormWrapper = styled.div`
+  width: 40%;
+  border-radius: 5px;
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+`;
+
+const CheckoutFormTitle = styled.h2`
+  margin-bottom: 3rem;
+`;
+
+const SaveShipping = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+  margin-bottom: 2rem;
+`;
+
+export default ShippingForm;
