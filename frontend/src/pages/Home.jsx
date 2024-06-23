532aca3ab37a5fee4f6cfb348445efe4efc01499
Ahmidou EL mehdi <ahmidouem@gmail.com>
2024-06-23 22:28:17 +0100

    Modified `Home.jsx` to include and style the `FlashSales` component

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,18 +1,20 @@
 import React from "react";
-import Products from "../components/products/Products";
 import Filter from "../components/ui/Filter";
 import styled from "styled-components";
 import Slide from "../components/slide/Slide";
 import Meta from "../components/ui/Meta";
+import FlashSales from "../components/products/FlashSales";
 
 const HomeContainer = styled.div`
   min-height: 120vh;
+  padding: 2rem 2rem 10rem 2rem;
+  overflow-x: hidden;
 `;
 
 const Herder = styled.header`
   display: flex;
   gap: 2rem;
-  margin-bottom: 6rem;
+  margin-bottom: 12rem;
 `;
 
 const Container = styled.div`
@@ -29,8 +31,8 @@ function Home() {
           <Filter />
           <Slide />
         </Herder>
+        <FlashSales />
       </Container>
-      <Products />
     </HomeContainer>
   );
 }
