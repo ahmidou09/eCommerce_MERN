60a6d299421fa739b7c1977a216b53fb5867b177
Ahmidou EL mehdi <ahmidouem@gmail.com>
2024-06-25 14:24:54 +0100

    Adjust static asset serving paths based on environment

--- backend/server.js
+++ backend/server.js
@@ -39,7 +39,7 @@ app.get("/api/config/paypal", (req, res) => {
 // serve frontend
 if (process.env.NODE_ENV === "production") {
   const __dirname = path.resolve();
-  app.use("/uploads", express.static("/var/data/uploads"));
+  app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
   app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
   app.get("*", (req, res) => {
