a13003240fde1550cbb2dc69544e2167da07145a
Ahmidou EL mehdi <ahmidouem@gmail.com>
2024-06-25 14:16:06 +0100

    Reorganize middleware and routes in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,14 @@
 import path from "path";
 import express from "express";
-import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 dotenv.config();
 import connectDB from "./config/db.js";
-import cors from "cors";
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
-import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
 import uploadRoutes from "./routes/uploadRoutes.js";
+import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
 
 // port
 const port = process.env.PORT || 5000;
@@ -26,44 +25,31 @@ app.use(express.urlencoded({ extended: true }));
 // cookies parser middleware
 app.use(cookieParser());
 
-// cors middleware
-app.use(
-  cors({
-    origin: "https://ecommerce-mern-1-b1ux.onrender.com",
-    credentials: true,
-  })
-);
-
 // routes
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
 app.use("/api/upload", uploadRoutes);
 
-// serve static assets in production
-const __dirname = path.resolve();
-app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+// payment route
+app.get("/api/config/paypal", (req, res) => {
+  res.send({ clientId: process.env.PAYPAL_CLIENT_ID });
+});
 
 // serve frontend
 if (process.env.NODE_ENV === "production") {
+  const __dirname = path.resolve();
+  app.use("/uploads", express.static("/var/data/uploads"));
   app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-  // This should come before your catch-all route
-  app.get("/api/config/paypal", (req, res) => {
-    res.send({ clientId: process.env.PAYPAL_CLIENT_ID });
-  });
-
-  // Catch all route to serve index.html for any other routes
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
   });
 } else {
+  const __dirname = path.resolve();
+  app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
   app.get("/", (req, res) => {
     res.send("API is running...");
-  });
-
-  app.get("/api/config/paypal", (req, res) => {
-    res.send({ clientId: process.env.PAYPAL_CLIENT_ID });
   });
 }
 
