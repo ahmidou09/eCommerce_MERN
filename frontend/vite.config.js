f3beccc0b427ce17a0e1d1a4b9ca164139d76f01
Ahmidou EL mehdi <ahmidouem@gmail.com>
2026-02-17 13:19:54 +0000

    feat: Initialize frontend project with Vite, React, and essential dependencies.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import eslint from "vite-plugin-eslint";
 
 export default defineConfig({
-  plugins: [react(), eslint()],
+  plugins: [react(), eslint({ failOnError: false })],
   build: {
     rollupOptions: {
       output: {
